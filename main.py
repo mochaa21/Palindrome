6254c2bb3781d7e4c4a2ea003d3aabdb84bd9831
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-21 17:30:48 +0700

    update code, change for -> while. preventing logical bugs (kiri < kanan), and save data array.split()

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 def palindrome(array):
     kiri = 0
     kanan = len(array) - 1
-    for _ in array:
-        if array[kiri] == array[kanan]:
-            kiri += 1
-            kanan -= 1
-            if array[kiri] == array[kanan]:
-                return f"This is palindrome. Text: {array}"
-    return f"This is not a palindrome. Text: {array}"
+    while kiri < kanan:
+        if array[kiri] != array[kanan]:
+            return f"This is not a palindrome. Text: {array}"
+        kiri += 1
+        kanan -= 1
+    return f"This is palindrome. Text: {array}"
+    
 
 
 # array = ['M', 'A', 'L', 'A', 'M']
 array_2 = ['B', 'U', 'K', 'U']
 array_3 = ['R', 'A', 'D', 'A', 'R']
 array = input("Use a space to enter a word.\nYour input -> ")
-array.split()
+array = array.split()
 print(palindrome(array_2))
 print(palindrome(array_3))
 print(palindrome(array))
