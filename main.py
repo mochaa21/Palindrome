0956d80f7b77266a12666c0c78eaf4a1004cbbb1
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-21 17:16:24 +0700

    array with user input

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ def palindrome(array):
     return f"This is not a palindrome. Text: {array}"
 
 
-array = ['M', 'A', 'L', 'A', 'M']
-array_2 = ['B', 'U', 'K', 'U']
-array_3 = ['R', 'A', 'D', 'A', 'R']
+# array = ['M', 'A', 'L', 'A', 'M']
+# array_2 = ['B', 'U', 'K', 'U']
+# array_3 = ['R', 'A', 'D', 'A', 'R']
+array = input("Use a space to enter a word.\nYour input -> ")
+array.split()
 print(palindrome(array))
-print(palindrome(array_2))
-print(palindrome(array_3))
+# print(palindrome(array_2))
+# print(palindrome(array_3))
